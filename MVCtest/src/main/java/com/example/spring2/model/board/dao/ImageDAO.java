383529d9cd37ea4ac5c959a485bc9b71acea97b9
MIN HEE <mib2139@gmail.com>
2018-08-23 16:28:46 +0900

    asvals update
    asvals update

--- MVCtest/src/main/java/com/example/spring2/model/board/dao/ImageDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring2.model.board.dao;
-
-import com.example.spring2.model.board.dto.ImageVO;
-
-public interface ImageDAO {
-
-	void insertImg(ImageVO vo) throws Exception;
-
-}
